10c7af6a062dcaee3b07748cf95a6afde92b965c
Andrew Wong <aw236@duke.edu>
2014-12-08 02:03:44 -0500

    Reformatted code, added attributions, added new function, multiplot() for plotting multiple ggplot graphs.

--- functions.R
+++ functions.R
@@ -13,6 +13,7 @@ panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
 
 
 ## Check packages function.
+## The following was written by Dr. Colin Rundel of Duke University.
 check_packages = function(names)
 {
   for(name in names) {
@@ -24,8 +25,7 @@ check_packages = function(names)
 
 ## Create pairs.panels() function. Use on dataframe of continuous variables.
 ## http://musicroamer.com/blog/2011/01/16/r-tips-and-tricks-modified-pairs-plot/
-panel.cor.scale <- function(x, y, digits=2, prefix="", cex.cor)
-{
+panel.cor.scale <- function(x, y, digits=2, prefix="", cex.cor) {
   usr <- par("usr"); on.exit(par(usr))
   par(usr = c(0, 1, 0, 1))
   r = (cor(x, y,use="pairwise"))
@@ -35,8 +35,7 @@ panel.cor.scale <- function(x, y, digits=2, prefix="", cex.cor)
   text(0.5, 0.5, txt, cex = cex * abs(r))
 }
 
-panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
-{
+panel.cor <- function(x, y, digits=2, prefix="", cex.cor) {
   usr <- par("usr"); on.exit(par(usr))
   par(usr = c(0, 1, 0, 1))
   r = (cor(x, y,use="pairwise"))
@@ -46,8 +45,7 @@ panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
   text(0.5, 0.5, txt, cex = cex )
 }
 
-panel.hist <- function(x, ...)
-{
+panel.hist <- function(x, ...) {
   usr <- par("usr"); on.exit(par(usr))
   par(usr = c(usr[1:2], 0, 1.5) )
   h <- hist(x, plot = FALSE)
@@ -56,16 +54,66 @@ panel.hist <- function(x, ...)
   rect(breaks[-nB], 0, breaks[-1], y, col="cyan", ...)
 }
 
-pairs.panels <- function (x,y,smooth=TRUE,scale=FALSE)
-{if (smooth ){
-  if (scale) {
-    pairs(x,diag.panel=panel.hist,upper.panel=panel.cor.scale,lower.panel=panel.smooth)
+pairs.panels <- function (x,y,smooth=TRUE,scale=FALSE) {
+  if (smooth ){
+    if (scale) {
+      pairs(x,diag.panel=panel.hist,upper.panel=panel.cor.scale,lower.panel=panel.smooth)
+    }
+    else {
+      pairs(x,diag.panel=panel.hist,upper.panel=panel.cor,lower.panel=panel.smooth)
+    } #else {pairs(x,diag.panel=panel.hist,upper.panel=panel.cor,lower.panel=panel.smooth)
   }
-  else {pairs(x,diag.panel=panel.hist,upper.panel=panel.cor,lower.panel=panel.smooth)
-  } #else {pairs(x,diag.panel=panel.hist,upper.panel=panel.cor,lower.panel=panel.smooth)
-}
-else #smooth is not true
-{ if (scale) {pairs(x,diag.panel=panel.hist,upper.panel=panel.cor.scale)
-} else {pairs(x,diag.panel=panel.hist,upper.panel=panel.cor) }
-} #end of else (smooth)
+  else { #smooth is not true
+    if (scale) {
+      pairs(x,diag.panel=panel.hist,upper.panel=panel.cor.scale)
+    } 
+    else {
+      pairs(x,diag.panel=panel.hist,upper.panel=panel.cor) 
+    }
+  } #end of else (smooth)
 } #end of function
+
+## The following function is written by Winston Chang and was found at:
+## http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+## Multiple plot function
+## ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+## - cols:   Number of columns in layout
+## - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+## If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+## then plot 1 will go in the upper left, 2 will go in the upper right, and
+## 3 will go all the way across the bottom.
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
